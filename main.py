68439a9358d23a32c301a2b4d1800a509990eee4
karim bahri <karim-bahri-18@outlook.fr>
2020-10-24 16:09:57 +0100

    Create README.md

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+#usr/bin/python3
 from tkinter import Tk
 from window import Window
 
 if __name__ == "__main__":
     app = Tk()
-    app.configure(bg="#1E1E1E")
     w = Window(app)
     app.mainloop()
